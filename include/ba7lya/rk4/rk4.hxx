fb50ea97324d90ab72daa7a0190393ae490084a1
BA7LYA <1042140025@qq.com>
2025-07-24 01:56:13 +0800

    u
    Signed-off-by: BA7LYA <1042140025@qq.com>

--- include/ba7lya/rk4/rk4.hxx
+++ include/ba7lya/rk4/rk4.hxx
@@ -21,47 +21,32 @@ namespace ba7lya::rk4 {
 ///
 class rk4 {
 public:
-    /// @brief 默认构造函数
-    /// @details 默认微分方程为 dy/dx = x + y
-    /// @warning 默认微分方程仅供测试使用，实际使用时请提供自定义微分方程
-    rk4()
-        : ode_(std::bind(&rk4::ode, this, std::placeholders::_1, std::placeholders::_2)) {
-        std::cerr << "warning: no differential equation assigned." << std::endl;
-    }
-
     /// @brief 构造函数
-    /// @param de 外部提供的微分方程 dy/dx = f(x,y)
+    /// @param[in] ode 外部提供的微分方程 dy/dx = f(x,y)
     explicit rk4(std::function<double(double, double)> ode)
         : ode_(ode) {}
 
     /// @brief 析构函数
     ~rk4() {}
 
-    /// @param x0 初始x值
-    /// @param y0 初始y值
-    /// @param h 步长
-    /// @param steps 迭代次数
+    /// @param[in,out] x x值
+    /// @param[in,out] y y值
+    /// @param[in] h 步长
+    /// @param[in] steps 迭代次数
     /// @return
-    double operator()(const double x0, const double y0, const double h, const int steps) {
-        x = x0;
-        y = y0;
+    void operator()(double& x, double& y, const double h, const int steps) {
+        double k1 { 0.0 }, k2 { 0.0 }, k3 { 0.0 }, k4 { 0.0 };
         for (int i = 0; i < steps; i++) {
-            k1 = h * f(x, y);
-            k2 = h * f(x + h / 2, y + k1 / 2);
-            k3 = h * f(x + h / 2, y + k2 / 2);
-            k4 = h * f(x + h, y + k3);
-            y += (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+            k1 = ode_(x, y);
+            k2 = ode_(x + h / 2, y + k1 / 2);
+            k3 = ode_(x + h / 2, y + k2 / 2);
+            k4 = ode_(x + h, y + h * k3);
+            y += (k1 + 2 * k2 + 2 * k3 + k4) * h / 6;
             x += h;
         }
-        return y;
     }
 
 private:
-    // 示例微分方程 dy/dx = f(x,y)
-    const double ode(const double x, const double y) const { return x + y; }
-
-    double x, y;
-    double k1, k2, k3, k4;
     std::function<double(double, double)> ode_; // dy/dx = f(x,y)
 };
 
